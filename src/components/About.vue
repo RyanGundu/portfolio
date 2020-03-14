e15b1e8d6cd0d48bdd58c548f4dbfd62bbb99b4d
Ryan Gundu <rgundu@uoguelph.ca>
2020-03-14 18:50:54 -0400

    4th year update

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,77 +1,81 @@
 <template>
   <div id="about">
-    <Diamond msg="About"/>
+    <Diamond msg="About" />
     <div class="cont">
-        <div class="row">
-            <div class="col-md-6">
-                <h2 class="text-center">Education</h2>
-                <div class="row">
-                    <div class="col-md-4">
-                        <img src="http://saknepal.org/wp-content/uploads/2015/05/University-of-Guelph1.jpg" class="img-fluid school-img" alt="Responsive image"/>
-                    </div>
-                    <div class="col-md-8">
-                        <p class="msg">
-                            <br>
-                            I have recently completed my 3rd year at the University of Guelph,
-                            studying both computer science and mathematics.
-                            <br><br>
-                            Object oriented programming (OOP), Data structures
-                            and Algorithms are a few of the many topics I have experience with.
-                            <br><br>
-                            A more recent topic of study for me is Artificial Intelligence (AI). I am
-                            very interested in the area of machine learning and more specifically
-                            the application of neural networks. 
-                        </p>
-                    </div>
-                </div>
+      <div class="row">
+        <div class="col-md-6">
+          <h2 class="text-center">Education</h2>
+          <div class="row">
+            <div class="col-md-4">
+              <img
+                src="http://saknepal.org/wp-content/uploads/2015/05/University-of-Guelph1.jpg"
+                class="img-fluid school-img"
+                alt="Responsive image"
+              />
             </div>
-            <div class="col-md-6">
-                <h2 class="text-center">Interests</h2>
-                <img src="../assets/AI.png" class="img-fluid hobbie-img" alt="Responsive image"/>
-                <img src="../assets/ball.png" class="img-fluid hobbie-img" alt="Responsive image"/>
-                <img src="../assets/pong.png" class="img-fluid hobbie-img2" alt="Responsive image"/>
+            <div class="col-md-8">
+              <p class="msg">
+                <br />I am currently completing my 4th year at the University of Guelph,
+                studying both computer science and mathematics.
+                <br />
+                <br />Object oriented programming (OOP), Data structures
+                and Algorithms are a few of the many topics I have experience with.
+                <br />
+                <br />A more recent topic of study for me is Artificial Intelligence (AI). I am
+                very interested in the area of machine learning and more specifically
+                the application of neural networks.
+              </p>
             </div>
+          </div>
         </div>
+        <div class="col-md-6">
+          <h2 class="text-center">Interests</h2>
+          <img src="../assets/AI.png" class="img-fluid hobbie-img" alt="Responsive image" />
+          <img src="../assets/ball.png" class="img-fluid hobbie-img" alt="Responsive image" />
+          <img src="../assets/pong.png" class="img-fluid hobbie-img2" alt="Responsive image" />
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import Diamond from './Diamond.vue'
+import Diamond from "./Diamond.vue";
 export default {
-  name: 'About',
+  name: "About",
   components: {
-      Diamond
+    Diamond
   }
-  
-}
+};
 </script>
 
 <style scoped>
 #about {
-    background-color: #EEEFEF;
+  background-color: #eeefef;
 }
 .cont {
-    max-width: 1920px;
-    margin: 0 auto;
+  max-width: 1920px;
+  margin: 0 auto;
 }
-.school-img, .hobbie-img,.hobbie-img2 {
-    display: block;
-    margin: 0 auto;
+.school-img,
+.hobbie-img,
+.hobbie-img2 {
+  display: block;
+  margin: 0 auto;
 }
 .hobbie-img {
-    max-width: 153px;
-    padding: 5px;
+  max-width: 153px;
+  padding: 5px;
 }
 .hobbie-img2 {
-    max-width: 145px;
-    padding: 5px;
+  max-width: 145px;
+  padding: 5px;
 }
 
 .msg {
-    padding: 20px;
-    max-width: 900px;
-    font-family: "Courier New", Courier, monospacel;
-    font-weight: 900;
+  padding: 20px;
+  max-width: 900px;
+  font-family: "Courier New", Courier, monospacel;
+  font-weight: 900;
 }
 </style>
--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,83 +1,180 @@
 <template>
-    <div id="skills">
-        <br>
-        <div class="row">
-            <div class="skill-bars col-lg-6">
-                <h1 class="text-center">SKILLS</h1><br>
+  <div id="skills">
+    <br />
+    <div class="row">
+      <div class="skill-bars col-lg-6">
+        <h1 class="text-center">SKILLS</h1>
+        <br />
 
-                <h3>Languages</h3> 
-                <v-progress-circular class="skill" :rotate="-90" :size="100" :width="15" :value="93" color="#3e67bc"> C </v-progress-circular>
-                <v-progress-circular class="skill" :rotate="-90" :size="100" :width="15" :value="88" color="#3e67bc"> Java </v-progress-circular>
-                <v-progress-circular class="skill" :rotate="-90" :size="100" :width="15" :value="88" color="#3e67bc"> JavaScript </v-progress-circular>
-                <v-progress-circular class="skill" :rotate="-90" :size="100" :width="15" :value="72" color="#3e67bc"> Python </v-progress-circular>
-                <br><br>
-                <h3>Frameworks</h3>
-                <v-progress-circular class="skill" :rotate="-90" :size="100" :width="15" :value="87" color="teal"> React </v-progress-circular>
-                <v-progress-circular class="skill" :rotate="-90" :size="100" :width="15" :value="85" color="teal"> Angular </v-progress-circular>
-                <v-progress-circular class="skill" :rotate="-90" :size="100" :width="15" :value="73" color="teal"> Vue </v-progress-circular>
-                <br><br>
-                <h3>Technologies</h3>
-                <v-progress-circular class="skill" :rotate="-90" :size="100" :width="15" :value="84" color="pink"> Node </v-progress-circular>
-                <v-progress-circular class="skill" :rotate="-90" :size="100" :width="15" :value="72" color="pink"> GraphQL </v-progress-circular>
-                <v-progress-circular class="skill" :rotate="-90" :size="100" :width="15" :value="76" color="pink"> Firebase </v-progress-circular>
-                <v-progress-circular class="skill" :rotate="-90" :size="100" :width="15" :value="70" color="pink"> MySQL </v-progress-circular>
-                <v-progress-circular class="skill" :rotate="-90" :size="100" :width="15" :value="73" color="pink"> MongoDB </v-progress-circular>
-                
-            </div>
-            <div class="experience col-lg-6">
-                <h1 class="text-center">EXPERIENCE</h1>
-                <img src="https://postmediawindsorstar2.files.wordpress.com/2014/04/rbc-royal-bank-logo.png?w=1200" class="img-fluid work-img" alt="Responsive image"/>
-                <div class="job-tag text-center">
-                    <p>
-                        Web Developer, WMAI Applied Analytics & Innovation <br>
-                        May 2019 - Present
-                    </p>
-                </div>
-                <br><hr>
-                <img src="https://ryangundu.github.io/images/pandanusLOGO.png" class="img-fluid work-img" alt="Responsive image"/>
-                <div class="job-tag text-center">
-                    <p>Web Developer<br>May 2018 - July 2018</p>
-                </div>
-            </div>
+        <h3>Languages</h3>
+        <v-progress-circular
+          class="skill"
+          :rotate="-90"
+          :size="100"
+          :width="15"
+          :value="93"
+          color="#3e67bc"
+        >C</v-progress-circular>
+        <v-progress-circular
+          class="skill"
+          :rotate="-90"
+          :size="100"
+          :width="15"
+          :value="88"
+          color="#3e67bc"
+        >Java</v-progress-circular>
+        <v-progress-circular
+          class="skill"
+          :rotate="-90"
+          :size="100"
+          :width="15"
+          :value="88"
+          color="#3e67bc"
+        >JavaScript</v-progress-circular>
+        <v-progress-circular
+          class="skill"
+          :rotate="-90"
+          :size="100"
+          :width="15"
+          :value="88"
+          color="#3e67bc"
+        >Python</v-progress-circular>
+        <br />
+        <br />
+        <h3>Frameworks</h3>
+        <v-progress-circular
+          class="skill"
+          :rotate="-90"
+          :size="100"
+          :width="15"
+          :value="87"
+          color="teal"
+        >React</v-progress-circular>
+        <v-progress-circular
+          class="skill"
+          :rotate="-90"
+          :size="100"
+          :width="15"
+          :value="85"
+          color="teal"
+        >Angular</v-progress-circular>
+        <v-progress-circular
+          class="skill"
+          :rotate="-90"
+          :size="100"
+          :width="15"
+          :value="73"
+          color="teal"
+        >Vue</v-progress-circular>
+        <br />
+        <br />
+        <h3>Technologies</h3>
+        <v-progress-circular
+          class="skill"
+          :rotate="-90"
+          :size="100"
+          :width="15"
+          :value="84"
+          color="pink"
+        >Node</v-progress-circular>
+        <v-progress-circular
+          class="skill"
+          :rotate="-90"
+          :size="100"
+          :width="15"
+          :value="72"
+          color="pink"
+        >GraphQL</v-progress-circular>
+        <v-progress-circular
+          class="skill"
+          :rotate="-90"
+          :size="100"
+          :width="15"
+          :value="76"
+          color="pink"
+        >Firebase</v-progress-circular>
+        <v-progress-circular
+          class="skill"
+          :rotate="-90"
+          :size="100"
+          :width="15"
+          :value="70"
+          color="pink"
+        >MySQL</v-progress-circular>
+        <v-progress-circular
+          class="skill"
+          :rotate="-90"
+          :size="100"
+          :width="15"
+          :value="73"
+          color="pink"
+        >MongoDB</v-progress-circular>
+      </div>
+      <div class="experience col-lg-6">
+        <h1 class="text-center">EXPERIENCE</h1>
+        <img
+          src="https://postmediawindsorstar2.files.wordpress.com/2014/04/rbc-royal-bank-logo.png?w=1200"
+          class="img-fluid work-img"
+          alt="Responsive image"
+        />
+        <div class="job-tag text-center">
+          <p>
+            Web Developer, WMAI Applied Analytics & Innovation
+            <br />May 2019 - August 2019
+          </p>
         </div>
-        
+        <br />
+        <hr />
+        <img
+          src="https://ryangundu.github.io/images/pandanusLOGO.png"
+          class="img-fluid work-img"
+          alt="Responsive image"
+        />
+        <div class="job-tag text-center">
+          <p>
+            Web Developer
+            <br />May 2018 - July 2018
+          </p>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'Skills'
-}
+  name: "Skills"
+};
 </script>
 
 <style scoped>
-
 #skills {
-    max-width: 1920px;
-    margin: 0 auto;
-    overflow-x: hidden;
+  max-width: 1920px;
+  margin: 0 auto;
+  overflow-x: hidden;
 }
 
 .row {
-    margin: 20px;
+  margin: 20px;
 }
 .skill {
-    margin: 5px;
+  margin: 5px;
 }
-.skill-bars, .experience {
-    padding: 10px;
+.skill-bars,
+.experience {
+  padding: 10px;
 }
 
 .work-img {
-    width: 350px;
-    margin: 0 auto;
-    margin-top: 70px; 
-    display: block;
+  width: 350px;
+  margin: 0 auto;
+  margin-top: 70px;
+  display: block;
 }
 
 .job-tag {
-    margin-top: 8px;
-    font-weight: bold;
+  margin-top: 8px;
+  font-weight: bold;
 }
 </style>
 
